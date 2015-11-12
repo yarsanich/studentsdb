7d1c898a127e2fc826cbb2217ffd9bb296db7956
Yaroslav Neznaradko <nezn27@yahoo.com>
2015-11-12 21:41:59 +0200

    MYSQL!

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -58,15 +58,8 @@ WSGI_APPLICATION = 'studentsdb.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '..', 'db.sqlite3'),
-    }
-}
-
-
+from .db import DATABASES
+#WE MOVED DATABASE TO db.py
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
